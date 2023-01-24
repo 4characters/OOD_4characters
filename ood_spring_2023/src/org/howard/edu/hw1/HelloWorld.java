7969303bd12b64dd2917a023cbc46265ed898377
Manish <manish.bhurtel09@gmail.com>
2023-01-24 13:19:39 -0500

    Added print statement

--- ood_spring_2023/src/org/howard/edu/hw1/HelloWorld.java
+++ ood_spring_2023/src/org/howard/edu/hw1/HelloWorld.java
@@ -1,10 +1,10 @@
 package org.howard.edu.hw1;
 
-// Author Name: Manish Bhurtel (@02964727)
+// Name: Manish Bhurtel (@02964727)
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Hello World!!!");
 	}
 }
