6f5a6e14cbfb74661bb34bf26bc18c0cd2920bbb
Manish <manish.bhurtel09@gmail.com>
2023-02-02 15:02:06 -0500

    Optimized the code

--- ood_spring_2023/src/org/howard/edu/hw2/WordCounter.java
+++ ood_spring_2023/src/org/howard/edu/hw2/WordCounter.java
@@ -5,7 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
+import java.io.FileNotFoundException;
 
 // Name: Manish Bhurtel (@02964727)
 
@@ -15,7 +15,7 @@ public class WordCounter {
 		// Creating the HashMap object to store the word as key and count as value
 		Map<String, Integer> wordsMap = new HashMap<>();
 		
-		// Specifying the location of the file
+		// Specifying the relative path of the file
 		String filepath = "src/main/resources/input.txt";
 		
 		System.out.println("The input text in the file is as follows:\n");
@@ -29,25 +29,27 @@ public class WordCounter {
 			while ((line = br.readLine()) != null) {
 				System.out.println(line);
 				
-				// Splitting the string 'line' to lower case and splitting using a space
+				// Converting the string 'line' to lower case and splitting using a space
 				String[] splitLine = line.toLowerCase().split(" ");
 				
 				// Now we loop over each word in the split line
 				for (String word : splitLine) {
 					
-					// We exclude the word that does not contain alphabet or has length <= 3
-					if (word.matches("[^a-zA-Z]*") || (word.length() <= 3)) {
-						continue;
-					}
-					
-					// Here we check whether the word is already present in the HashMap or not
-					if (!wordsMap.containsKey(word)) {
-						wordsMap.put(word, 1);	// Put value = 1 if the word is not in HashMap
-						} else {
-							wordsMap.put(word, wordsMap.get(word) + 1);
-						}	// If word is present in the HashMap, then increase the count by 1
+					// We count only valid words with length > 3
+					if (!word.matches("[^a-zA-Z]*") && (word.length() > 3)) {
+						
+						// Here we check whether the word is already present in the HashMap or not
+						if (!wordsMap.containsKey(word)) {
+							wordsMap.put(word, 1);	// Put value = 1 if the word is not in HashMap
+							} else {
+								wordsMap.put(word, wordsMap.get(word) + 1);
+							}	// If word is present in the HashMap, then increase the count by 1
+						}
 					}
 				}
+			// Catch the exception if file is not found
+			} catch (FileNotFoundException exp) {
+				System.out.println(filepath + "\nNo such file in the working director!");
 			}
 		System.out.println("--------------------------------------------------------------------------------------------");
 		System.out.println("\nThe count of each unique word is shown below:");
